b1dfb6d18f76ef7b8f533064ea2d092723ea902e
Clapfix <ronny.perner@kabelmail.de>
2022-01-31 12:17:57 +0100

    1.1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,14 +3,28 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        Cat cat1 = new Cat("Grizabella","white",29);
         
-        // Cat --> Referenztyp / reference type
-        // cat --> Referenzvariable / reference variable
-        // new Cat() --> Call des default-Konstruktors
-        Cat cat = new Cat();
-        System.out.println(cat);
-        
+        cat1.tellYourName();
+        cat1.tellYourFurColor();
+        cat1.tellYorAge();
+
+        System.out.println("------------------");
+
+        // Cat cat2 = new Cat();
+        // cat2.name = "Alonzo";
+        // cat2.furColor = "grey";
+        // cat2.age = 35;
+
+        // cat2.tellYourName();
+        // cat2.tellYourFurColor();
+        // cat2.tellYorAge();
+    
     }
 
 }
 
+// Cat --> Referenztyp / reference type
+        // cat --> Referenzvariable / reference variable
+        // new Cat() --> Call des default-Konstruktors
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,11 +1,28 @@
 package com.cc.java;
 
 public class Cat {
+
+    public String name;
+    public String furColor;
+    public int age;
+
+    Cat (String name, String furColor, int age){
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+
+
+    public void tellYourName(){
+        System.out.println(this.name);
+    }
+
+    public void tellYourFurColor() {
+        System.out.println(this.furColor);
+    }
     
-void tellYouAddress(){
-    System.out.println("hi");
-
-}
-
-
+    public void tellYorAge() {
+        System.out.println(this.age);       
+    }
 }
