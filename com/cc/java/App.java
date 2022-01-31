074f19f00109a62a85d55a70672ed69b3c5ee1e4
Clapfix <ronny.perner@kabelmail.de>
2022-01-31 12:29:31 +0100

    1.2

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,21 +5,16 @@ public class App {
     public static void main(String[] args) {
 
         Cat cat1 = new Cat("Grizabella","white",29);
-        
         cat1.tellYourName();
         cat1.tellYourFurColor();
         cat1.tellYorAge();
 
         System.out.println("------------------");
 
-        // Cat cat2 = new Cat();
-        // cat2.name = "Alonzo";
-        // cat2.furColor = "grey";
-        // cat2.age = 35;
-
-        // cat2.tellYourName();
-        // cat2.tellYourFurColor();
-        // cat2.tellYorAge();
+        Cat cat2 = new Cat("Alonzo","grey",35);
+        cat2.tellYourName();
+        cat2.tellYourFurColor();
+        cat2.tellYorAge();
     
     }
 
